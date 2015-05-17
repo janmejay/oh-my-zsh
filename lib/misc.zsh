e933432a47e62a02b97285ca1d33ba33b52078db
Janmejay Singh <singh.janmejay@gmail.com>
2015-05-17 20:03:22 +0530

    grep issues warning without this

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -46,7 +46,7 @@ alias history='fc -l 1'
 
 ## Color grep results
 ## Examples: http://rubyurl.com/ZXv
-export GREP_OPTIONS='--color=auto'
+
 export GREP_COLOR='1;32'
 
 # Quick and dirty encryption
